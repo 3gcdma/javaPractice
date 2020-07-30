40842d51a50452479344c87cf53295cfe7c7bc5f
kims <kshwnahr@gmail.com>
2020-07-30 20:07:33 +0900

    재귀 연습 - PowerSet

---
+++ monthend/day0730/S1.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class S1 {
+
+	static int N, totalCnt;
+	static int[] input;
+	static boolean[] isSelected;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		input = new int[N];
+		isSelected = new boolean[N];
+		for(int i = 0; i < N; i++) {
+			input[i] = sc.nextInt();
+		}
+		generateSubset(0);
+		System.out.println("총 경우의 수 : " +totalCnt);
+	}
+
+	private static void generateSubset(int cnt) {
+		if(cnt == N) {
+			++totalCnt;
+			for(int i = 0; i < N; i++) {
+				System.out.print(isSelected[i] ? input[i] : "X");
+				System.out.print("\t");
+			}
+			System.out.println();
+			return;
+		}
+		
+		//반복
+		//부분집합 구성에 포함
+		isSelected[cnt] = true;
+		generateSubset(cnt+1);
+		//부분집합 구성에 비포함
+		isSelected[cnt] = false;
+		generateSubset(cnt+1);
+	}
+}
