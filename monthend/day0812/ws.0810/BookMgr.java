033b143638546eaa0631b354bf917762fa0cd1f5
3gcdma <kshwnahr@gmail.com>
2020-08-12 18:48:17 +0900

    S 전자 회사 도서관 관리 프로그램  : 상속 및 다형성 연습, DAO, DTO

---
+++ monthend/day0812/ws.0810/BookMgr.java
@@ -0,0 +1,152 @@
+﻿import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BookMgr {
+
+	private static BookMgr instance;
+	private Book[] books = new Book[2];
+	//private Book books = new Book[2];
+	//데이터가 들어갈 위치아 입력된 데이터의 크기
+	private int index;
+	
+	private BookMgr() {
+
+	}
+
+	public static BookMgr getInstance() {
+		if (instance == null)
+			instance = new BookMgr();
+		return instance;
+	}
+
+	/** 파라메터로 전달된 화 정보를 전달받아 배열에 저장한다. */
+	public void add(Book book) {
+		
+		//1
+		if(index == books.length) {
+			books = Arrays.copyOf(books, index * 2);
+		}
+		books[index++] = book;
+		
+		//System.out.println(Arrays.toString(movies));
+	}
+
+	/** 배열에 저장된 모든 화정보를 리턴한다. */
+	public Book[] searchAllBooks() {
+		return Arrays.copyOf(books, index);
+	}
+	
+	public Book selectISBN(String isbn) {
+		for(int i = 0; i < index; i++) {
+			if(books[i].getIsbn().equals(isbn)) {
+				Book b = books[i];
+				return b;
+			}
+		}
+		return null;
+		//for문을 통해 해당하는 값을 전달
+	}
+	/** 파라메터로 전달된 타이틀을 포함하고 있는 모든 화 정보를 검색하여 리턴한다 */
+	public Book[] selectTitle(String title) {
+		int idx = 0;
+		Book[] b = new Book[index];
+		//3
+		for(int i = 0; i < index; i++) {
+			if(books[i].getTitle().contains(title)) {
+				b[idx++] = books[i];
+			}
+		}
+		//4 새롭게 생성된 배열의 주소를 기본 변수에 대입
+		return Arrays.copyOf(b, idx);
+	}
+	
+	public Book[] selectBooks() {
+		int idx = 0;
+		Book[] b = new Book[index];
+
+		for(int i = 0; i < index; i++) {
+			///if(books[i] instanceof Book) { ///  둘다 참이다 
+			if(books[i] instanceof Megazine) continue;
+			
+			b[idx++] = books[i];
+		}
+
+		return Arrays.copyOf(b, idx);
+	}
+	
+	public Book[] selectMegazines() {
+		int idx = 0;
+		Book[] b = new Book[index];
+
+		for(int i = 0; i < index; i++) {
+			if(books[i] instanceof Megazine) b[idx++] = books[i];
+		}
+
+		return Arrays.copyOf(b, idx);
+	}
+
+//	/** 파라메터로 전달된 감독명의 모든 화 정보를 검색하여 리턴한다 */
+//	public Movie searchDirector(String name) {
+//		//3
+//		for(int i = 0; i < index; i++) {
+//			if(movies[i].getDirector().equals(name)) {
+//				return movies[i];
+//			}
+//		}
+//		//4 새롭게 생성된 배열의 주소를 기본 변수에 대입
+//		return null;
+//	}
+//
+//	/** 파라메터로 전달된 장르의 모든 화 정보를 검색하여 리턴한다 */
+//	public Movie[] searchGenre(String genre) {
+//		int idx = 0;
+//		Movie[] temp = new Movie[index];
+//		//3
+//		for(int i = 0; i < index; i++) {
+//			if(movies[i].getGenre().equals(genre)) {
+//				temp[idx++] = movies[i];
+//			}
+//		}
+//		//4 새롭게 생성된 배열의 주소를 기본 변수에 대입
+//		return Arrays.copyOf(temp, idx);
+//	}
+//
+//	/** 파라메터로 전달된 제목의 화를 삭제한다. */
+//	public void delete(String title) {
+//		
+//		////내가 쓴 코드
+////		int idx = 0;
+////		for(int i = 0; i < index; i++) {
+////			if(movies[i].getTitle().equals(title)) {
+////				idx = i;
+////				break;
+////			}
+////		}
+////		
+////		for(int i = idx; i < index; i++) {
+////			movies[i] = movies[i+1];
+////		}
+////		movies[index] = null;
+////		index--;
+//		
+//		////조언 코드(배열 통째로 복사)
+//		for(int i = 0; i < index; i++) {
+//			if(movies[i].getTitle().equals(title)) {
+//				int movecnt = index - i + 1;
+//				if(movecnt != 0) {
+//					System.arraycopy(movies, i+1, movies, i, index - i + 1);
+//				}
+//				movies[index--] = null;
+//				break;
+//			}
+//		}
+//		
+//	}
+//
+	/** 저장된 화 정보의 갯수를 리턴한다. */
+	public int getSize() {
+		return index;
+	}
+
+
+}
---
+++ monthend/day0812/ws.0810/BookTest.java
@@ -0,0 +1,217 @@
+﻿import java.util.Scanner;
+
+public class BookTest {
+	private Scanner sc = new Scanner(System.in);
+	private BookMgr mgr = BookMgr.getInstance();
+	
+	private void service() {
+		System.out.println("<<< 영화 관리 프로그램 >>> ");
+		
+		
+		while(true) {
+			int menu = showMenu();
+			switch(menu) {
+			case 1:
+				addBook();
+				break;
+			case 2:
+				showAllBooks();
+				break;
+			case 3:
+				showBookbyISBN();
+				break;
+			case 4:
+				selectTitle();
+				break;
+			case 5:
+				selectBooks();
+				break;
+			case 6:
+				selectMegazines();
+				break;
+			case 0:
+				System.out.println("종료합네다");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("잘못된 선택입네다. 다시 고루시라우요");
+			}
+		}
+	}
+
+
+//	
+//	private void deleteMovie() {
+//		// TODO Auto-generated method stub
+//		System.out.print("제목을 입력하세요 : ");
+//		String title = sc.nextLine();
+//		mgr.delete(title);
+//		System.out.println("------------------------------");
+//		System.out.println("삭제가 완료되었습니다.");
+//		System.out.println("------------------------------");
+//	}
+//
+//	private void searchGen() {
+//		// TODO Auto-generated method stub
+//		System.out.print("제목을 입력하세요 : ");
+//		String genre = sc.nextLine();
+//		Movie[] movies = mgr.searchGenre(genre);
+//		System.out.println("------------------------------");
+//		System.out.println("검색 정보");
+//		System.out.println("------------------------------");
+//		System.out.println("제목\t감독\t장르\t등급\t내용");
+//		for(Movie m : movies) {
+//			System.out.println(m.getTitle() + "\t" + m.getDirector() + "\t" + m.getGenre() + "\t" + m.getGrade() + "\t" + m.getSummary());
+//			//System.out.println(m);
+//		}
+//		System.out.println("------------------------------");
+//	}
+//
+//	private void searchDir() {
+//		// TODO Auto-generated method stub
+//		System.out.print("감독 이름을 입력하세요 : ");
+//		String name = sc.nextLine();
+//		Movie m = mgr.searchDirector(name);
+//		if(m == null) {
+//			System.out.println(name + " 감독의 작품이 조회되지 않음!!");
+//			return;
+//		}
+//		System.out.println("------------------------------");
+//		System.out.println("검색 정보");
+//		System.out.println("------------------------------");
+//		System.out.println("제목\t감독\t장르\t등급\t내용");
+//		System.out.println(m.getTitle() + "\t" + m.getDirector() + "\t" + m.getGenre() + "\t" + m.getGrade() + "\t" + m.getSummary());
+//			//System.out.println(m);
+//		System.out.println("------------------------------");
+//	}
+//
+	
+	private void selectMegazines() {
+		System.out.println("매거진만 검색");
+		System.out.println("------------------------------");
+		System.out.println("검색 정보");
+		System.out.println("------------------------------");
+		printBooks(mgr.selectMegazines());
+		System.out.println("------------------------------");
+		
+	}
+
+	private void selectBooks() {
+		System.out.println("책만 검색");
+		
+		System.out.println("------------------------------");
+		System.out.println("검색 정보");
+		System.out.println("------------------------------");
+		//System.out.println("제목\t감독\t장르\t등급\t내용");
+		printBooks(mgr.selectBooks());
+		System.out.println("------------------------------");
+			
+	}
+
+	private void selectTitle() {
+		// TODO Auto-generated method stub
+		System.out.println("책 제목 검색");
+		String title = scanString("제목을 입력하세요 : ");
+		
+		System.out.println("------------------------------");
+		System.out.println("검색 정보");
+		System.out.println("------------------------------");
+		//System.out.println("제목\t감독\t장르\t등급\t내용");
+		printBooks(mgr.selectTitle(title));
+		System.out.println("------------------------------");
+	}
+
+	private void printBooks(Book[] books) {
+		for(Book b : books) {
+			//System.out.println(m.getTitle() + "\t" + m.getDirector() + "\t" + m.getGenre() + "\t" + m.getGrade() + "\t" + m.getSummary());
+			System.out.println(b);
+		}
+	}
+	private void showBookbyISBN() {
+		System.out.println("책 ISBN 정보 검색");
+		String isbn = scanString("검색할 ISBN 번호 : ");
+		Book book = mgr.selectISBN(isbn);
+		if(book == null) {
+			System.out.println("입력한 ISBN에 해당하는 책이 없습니다.");
+			return;
+		}
+		System.out.println(book);
+	}
+	private void showAllBooks() {
+		System.out.println("------------------------------");
+		System.out.println("도서 정보 (전체 : " + mgr.getSize() + "권)");
+		System.out.println("------------------------------");
+		//System.out.println("제목\t감독\t장르");
+		printBooks(mgr.searchAllBooks());
+		System.out.println("------------------------------");
+		System.out.println("------------------------------");
+		
+	}
+
+	private int showMenu() {
+		System.out.println("------------------------------");
+		System.out.println("------------------------------");
+		System.out.println("1.책 정보 입력 ");
+		System.out.println("2.도서 정보 전체 검색 ");
+		System.out.println("3.도서 ISBN 검색 ");
+		System.out.println("4.도서 제목 검색 ");
+		System.out.println("5.책만 검색 ");
+		System.out.println("6.잡지만 검색 ");
+//		System.out.println("5.영화 정보 삭제 ");
+//		System.out.println("6.영화 감독 검색");
+		System.out.println("0.종료 ");
+		System.out.println("------------------------------");
+		System.out.println("------------------------------");
+		System.out.print("원하는 번호를 선택하세요. : ");
+		int menu = Integer.parseInt(sc.nextLine());
+		return menu;
+	}
+	
+	private void addBook() {
+
+		System.out.println("책 정보 입력");
+		
+		int kind = scanInt("선택(1.책, 2.잡지) : ");
+		String isbn = scanString("책번호(ISBN) : ");
+		String title = scanString("제목 : ");
+		String author = scanString("저자 : ");
+		String publisher = scanString("출판사 : ");
+		int price = scanInt("가격 : ");
+		String desc = scanString("설명 : ");
+
+		
+		if(kind == 1) {
+			mgr.add(new Book(isbn, title, author, publisher, price, desc));
+		}else {
+			int year = scanInt("년도 : ");
+			int month = scanInt("월 : ");
+			mgr.add(new Megazine(isbn, title, author, publisher, price, desc, year, month));
+		}
+		
+
+		System.out.println("영화 정보가 등록되었습니다.");
+		
+
+		//System.out.print("제목 : ");
+		//String title = sc.nextLine();
+		//m.setTitle(title);
+		
+	}
+	
+	private String scanString(String text) {
+		System.out.print(text);
+		return sc.nextLine();
+	}
+	
+	private int scanInt(String text) {
+		return Integer.parseInt(scanString(text));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BookTest mt = new BookTest();
+		mt.service();
+		
+	}
+	
+}
