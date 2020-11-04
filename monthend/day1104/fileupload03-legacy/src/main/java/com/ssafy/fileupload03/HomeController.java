a0fe4598fee6892b6607f57ce5f9d9bfea12e05e
3gcdma <kshwnahr@gmail.com>
2020-11-04 17:49:58 +0900

    spring 복습

---
+++ monthend/day1104/fileupload03-legacy/src/main/java/com/ssafy/fileupload03/HomeController.java
@@ -0,0 +1,17 @@
+package com.ssafy.fileupload03;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+}
---
+++ monthend/day1104/fileupload05-board/src/main/java/com/ssafy/fileupload05/board/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.ssafy.fileupload05.board.controller;
